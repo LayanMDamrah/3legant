effd1b821fa7f15122cacc9f5b5074fcb99b3385
Maha Azzouni <azzounimaha54321@gmail.com>
2025-12-06 16:23:01 +0200

    php test2

--- php/login.php
+++ php/login.php
@@ -1,30 +1,37 @@
 <?php
-require_once("tools.php");
+session_start();
+require_once("../tools.php");
 
 $conn = Database::connect();
 
-    if($_SERVER["REQUEST_METHOD"] == "POST"){
-        $username = filter_input(INPUT_POST, "username", FILTER_SANITIZE_SPECIAL_CHARS);
-        $password = filter_input(INPUT_POST, "password", FILTER_SANITIZE_SPECIAL_CHARS);
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
-        if(empty($username)){
-            echo "Please enter the UserName";
-        }
-        if(empty($password)){
-            echo "Please enter the Password";
-        }
+    $username = filter_input(INPUT_POST, "username", FILTER_SANITIZE_SPECIAL_CHARS);
+    $password = filter_input(INPUT_POST, "password", FILTER_SANITIZE_SPECIAL_CHARS);
 
-        else {
-            $hash = password_hash($password, PASSWORD_DEFAULT);
-            $sql = "INSERT INTO users (user, password)
-                    VALUES ('$username', '$hash')";
-            try{
-                mysqli_query($conn, $sql);
-                echo "You are now registered!";
-            }catch(mysqli_sql_exception){
-                echo " This name already in the database";
-            }
-            
-        }
+    if (empty($username) || empty($password)) {
+        echo "All fields are required!";
+        exit();
     }
+
+    // Fetch user from database
+    $sql = "SELECT * FROM users WHERE user = '$username' LIMIT 1";
+    $result = mysqli_query($conn, $sql);
+
+    if ($result && mysqli_num_rows($result) === 1) {
+        $row = mysqli_fetch_assoc($result);
+
+        if (password_verify($password, $row['password'])) {
+            // login success
+            $_SESSION['user'] = $row['user'];
+            header("Location: ../homepage.php");
+            exit();
+        } else {
+            echo "Incorrect password!";
+        }
+
+    } else {
+        echo "User not found!";
+    }
+}
 ?>
